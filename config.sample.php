3b67c77c8138d8a1f9e638086782860b250c98d6
Audi Lu <khl0327@gmail.com>
2020-09-30 14:00:09 +0800

    update config sample

--- config.sample.php
+++ config.sample.php
@@ -2,16 +2,19 @@
 define ('DEV', 0);
 define ('PRO', 1);
 $conf = array();
+$conf[DEV]['ConfigMode'] = 'stage';
 $conf[DEV]['ServiceURL'] = 'https://payment-stage.ecpay.com.tw/MerchantMember/';
 $conf[DEV]['HashKey'] = 'pwFHCqoQZGmho4w6';
 $conf[DEV]['HashIV'] = 'EkRm7iFT261dpevs'; 
 $conf[DEV]['MerchantID'] = '3002607';
 
+$conf[PRO]['ConfigMode'] = 'production';
 $conf[PRO]['ServiceURL'] = 'https://payment.ecpay.com.tw/MerchantMember/';
 $conf[PRO]['HashKey'] = '';
 $conf[PRO]['HashIV'] = '';
 $conf[PRO]['MerchantID'] = '';
 
+// 使用正式(PRO)或測試(DEV)設定
 $config = $conf[DEV];
 
 $http = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
