c72d2c5ff94d1761d4e7f3dfc49eb2dad63a8111
Audi Lu <khl0327@gmail.com>
2020-09-30 13:43:24 +0800

    ui

--- index.php
+++ index.php
@@ -2,49 +2,33 @@
 include_once('config.php');
 include_once('utils.php');
 
+// 取得從 user 填寫的 form 欄位資料
 $api = (isset($_POST['api'])) ? $_POST['api'] : '';
 $MerchantMemberID = (isset($_POST['MerchantMemberID'])) ? $_POST['MerchantMemberID'] : '';
 $CardID = (isset($_POST['CardID'])) ? $_POST['CardID'] : '';
 
+// 取得 config 設定值
 $MerchantID = $config['MerchantID'];
 $ServiceURL = $config['ServiceURL'].$api;
 $HashKey = $config['HashKey'];
 $HashIV = $config['HashIV'];
 
+// 進行綁定信用卡時要跳轉回來的頁面
 $ServerReplyURL = $cur_url.'/_server_reply.php';
 $ClientRedirectURL = $cur_url.'/_client_redirect.php';
 $ClientBackURL = $cur_url.'/_client_back.php';
 
+// 廠商交易編號，每次呼叫的值都必須不同 (timestamp)
 $MerchantTradeNo = round(microtime(true) * 1000);
 
+// 廠商交易日期 (綁定的時間)
 $MerchantTradeDate = date('Y/m/d h:i:s', time());// '2018/08/30 00:00:00';
+
+// 交易金額 (綁定金額)
 $TotalAmount = '5';
 $TradeDesc = 'Desc';
 
-$ary_use_fds = array();
-if (!empty($api)) {
-    switch ($api) {
-        case 'TradeWithBindingCardID':
-            $ary_use_fds = array( 
-                'MerchantID', 'MerchantMemberID', 
-                'ServerReplyURL', 'ClientRedirectURL', 'ClientBackURL',
-                'MerchantTradeNo', 'MerchantTradeDate', 'TotalAmount',
-                'TradeDesc', 'stage'
-            );
-        break;
-        case 'QueryMemberBinding':
-            $ary_use_fds = array( 
-                'MerchantID', 'MerchantMemberID'
-            );
-        break;
-        case 'DeleteCardID':
-            $ary_use_fds = array( 
-                'MerchantID', 'MerchantMemberID', 'CardID'
-            );
-        break;
-    }
-}
-
+// 收集上述資料，作為 API 呼叫所需的參數
 $params['MerchantID'] = $MerchantID;
 $params['MerchantMemberID'] = $MerchantMemberID;
 $params['ServerReplyURL'] = $ServerReplyURL;
@@ -57,12 +41,9 @@ $params['TradeDesc'] = $TradeDesc;
 $params['CardID'] = $CardID;
 $params['stage'] = '0';
 
-$use_params = array();
-foreach ($ary_use_fds as $fd) {
-    $use_params[$fd] = $params[$fd];
-}
-
-$mac = ECPayGenCheckMac($use_params, $HashKey, $HashIV, 1);
+// 呼叫 API 並取得結果
+// TBD: 綁定信用卡須跳轉到 ecpay
+$api_rst = fetch_api_rst($api, $params, $HashKey, $HashIV, $config['ServiceURL']);
 
 ?>
 <!DOCTYPE html>
@@ -76,51 +57,84 @@ $mac = ECPayGenCheckMac($use_params, $HashKey, $HashIV, 1);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <style>
+table .CheckMacValue{display: none;}
 </style>
 </head>
 <body>
     <div class="container">
-        <div class="row">
+        <div class="row mt-3">
             <div class="col-12">
-                <h1>ECPay API test</h1>
+                <h1>ECPay 不定期不定額 API demo</h1>
+                <div class="mb-3">
+                    設定檔：
+                    <span class="badge badge-primary"><?php echo $config['ConfigMode'];?></span>
+                </div>
                 <form method="post" class="mb-5" action="">
                     <label>API：</label>
                     <select name="api" class="form-control">
-                        <option value="TradeWithBindingCardID">綁定信用卡</option>
+                        <!--<option value="TradeWithBindingCardID">綁定信用卡</option>-->
                         <option value="QueryMemberBinding">查詢會員</option>
                         <option value="DeleteCardID">解除綁定信用卡</option>
                     </select>
                     <label>Merchant Member ID：(如：<?php echo $MerchantID;?>A0001) </label>
                     <input type="text" class="form-control" name="MerchantMemberID" value="<?php echo $MerchantMemberID;?>" placeholder="通常是 Merchant ID + 網站會員ID">
 
-                    <label>Merchant Trade No：(timestamp, 每次呼叫都要不同) </label>
-                    <input type="text" class="form-control" name="MerchantTradeNo" value="<?php echo $MerchantTradeNo;?>">
+                    <div class="d-none">
+                        <label>Merchant Trade No：(timestamp, 每次呼叫都要不同) </label>
+                        <input type="text" class="form-control" name="MerchantTradeNo" value="<?php echo $MerchantTradeNo;?>">
+                    </div>
 
-                    <label>Card ID：(解除綁定時填寫) </label>
-                    <input type="text" class="form-control" name="CardID" value="<?php echo $CardID;?>">
+                    <div class="cardid_wrap d-none">
+                        <label>Card ID：(解除綁定時必填) </label>
+                        <input type="text" class="form-control" name="CardID" value="<?php echo $CardID;?>">
+                    </div>
 
-                    <button type="submit" class="btn btn-secondary">取得參數</button>
+                    <button type="submit" class="mt-3 btn btn-secondary">呼叫 API</button>
                 </form>
 
-                <?php if(!empty($api) && !empty($MerchantMemberID)): 
-                    echo 'API: '.$api;
-                    ?>
-
-                <form id="__ecpayForm" method="post" target="_self" action="<?php echo $ServiceURL;?>">
-                <?php
-                foreach ($use_params as $fd => $val) {
-                    ?>
-                    <input type="hidden" class="form-control" name="<?php echo $fd?>" value="<?php echo $val;?>">
-                    <?php
-                }
-                ?>
-                <input type="hidden" name="CheckMacValue" value="<?php echo $mac;?>">
-                <input type="submit" class="btn btn-primary" id="__paymentButton" value="Send To Ecpay">
-                </form>
-
-                <?php endif; ?>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                <section id="api_result">
+                <?php 
+                // 輸出呼叫 API 的結果
+                if (!empty($api) && !empty($api_rst)) {
+                    if ($api !== 'TradeWithBindingCardID') {
+                        echo api_rst_to_html($api, $api_rst, 'all');
+                    }else{
+                        // TBD: 綁定信用卡須跳轉到 ecpay
+                        print_r($api_rst);
+                    }
+                }
+                ?>
+                </section>
+            </div>
+        </div>
+
     </div>
 </body>
+<script>
+$(function(){
+    // 若有選定的 API 就設為 select 的值
+    const api_sel = '<?php echo $api;?>';
+    if (api_sel) {
+        $('select[name=api]').val(api_sel);
+    }
+
+    // API select 變更選項
+    $(document).on('change', 'select[name=api]', function(e){
+        $('.cardid_wrap').addClass('d-none');
+        console.log($(this).val());
+        switch($(this).val()) {
+            // 選擇「解綁信用卡」就出現 CardID 欄位
+            case 'DeleteCardID':
+                $('.cardid_wrap').removeClass('d-none');
+                break;
+            default:
+                break;
+        }
+    });
+})
+</script>
 </html>
--- utils.php
+++ utils.php
@@ -1,4 +1,151 @@
 <?php
+//curl 取得遠端內容
+function curl_get_contents($url, $args=array(), $command='', $isPost = true) 
+{
+    if (!$isPost){
+        $i = 0;
+        foreach ($args as $key => $value){
+            if ($i<=0){$fg = '?';}
+            else{$fg = '&';}
+            $command .= $fg."$key=$value";
+            $i++;
+        }
+    }
+    //echo $command;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url.$command);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    if ($isPost){
+        curl_setopt($ch, CURLOPT_POST, true); // 啟用POST
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query( $args, '', '&')); //天殺的&amp;問題
+    }
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $r = curl_exec($ch);
+    if ($r === false) {
+        //wp_die('Curl error: ' . curl_error($ch) );
+        curl_close($ch);
+        return false;
+    }
+    else {
+        curl_close($ch);
+        return $r;
+    }
+}
+
+// 取得 api 要使用的欄位，並建立 ecpay mac
+function build_api_params($api, $params, $HashKey, $HashIV) {
+    // api 會使用到的欄位
+    $ary_use_fds = array();
+    switch ($api) {
+        // 綁定信用卡
+        case 'TradeWithBindingCardID':
+            $ary_use_fds = array( 
+                'MerchantID', 'MerchantMemberID', 
+                'ServerReplyURL', 'ClientRedirectURL', 'ClientBackURL',
+                'MerchantTradeNo', 'MerchantTradeDate', 'TotalAmount',
+                'TradeDesc', 'stage'
+            );
+        break;
+        // 查詢
+        case 'QueryMemberBinding':
+            $ary_use_fds = array( 
+                'MerchantID', 'MerchantMemberID'
+            );
+        break;
+        // 解綁
+        case 'DeleteCardID':
+            $ary_use_fds = array( 
+                'MerchantID', 'MerchantMemberID', 'CardID'
+            );
+        break;
+    }
+    // 取得 api 會用到的欄位設定值
+    $use_params = array();
+    foreach ($ary_use_fds as $fd) {
+        $use_params[$fd] = $params[$fd];
+    }
+
+    // 將 api 會用到的欄位值，拿來建立 ECPay Mac
+    $mac = ECPayGenCheckMac($use_params, $HashKey, $HashIV, 1);
+    $use_params['CheckMacValue'] = $mac;
+
+    return $use_params;
+}
+
+function api_rst_to_html($api, $rst, $mode = 'json') {
+
+    if ($mode === 'json') {
+        $json = $rst['JSonData'];
+        $json = json_decode($json, true);
+        $rst = $json;
+    }
+    // switch($api) {
+    //     case 'QueryMemberBinding':            
+    //     break;
+    // }
+
+    $html = '<table class="mt-2 table">';
+    // 標題列
+    $col_names = array_keys($rst);
+    $html .= '<tr>';
+    foreach ($col_names as $col) {
+        $html .= '<th class="'.$col.'">'.$col.'</th>';
+    }
+    $html .= '</tr>';
+
+    // 資料列
+    $col_vals = array_values($rst);
+    $html .= '<tr>';
+    foreach ($col_vals as $val) {
+        $col = array_search ($val, $rst);
+        $html .= '<td class="'.$col.'">';
+        // 若欄位是 JSonData 就另建 table 輸出
+        if ($col === 'JSonData') {
+            $json = json_decode($val, true);
+            $html .= '<table class="table json">';
+            foreach ($json as $jk => $jv) {
+                $html .= '<tr><th>'.$jk.'</th><td>'.$jv.'</td></tr>';
+            }
+            $html .= '</table>';
+        }else{
+            $html .= $val;
+        }
+        $html .= '</td>';
+    }
+    $html .= '</tr>';
+
+    $html .= '</table>';
+
+    return $html;
+}
+
+function fetch_api_rst($api, $params, $HashKey, $HashIV, $ServiceURL) {
+
+    $use_params = build_api_params($api, $params, $HashKey, $HashIV);
+
+    // 呼叫 ECpay API，取得回傳結果
+    $api_url = $ServiceURL . $api;
+    $rst_str = curl_get_contents($api_url, $use_params);
+
+    // 綁定信用卡
+    if ($api === 'TradeWithBindingCardID') {
+        return $rst_str;
+    }
+
+    $ary_rst_str = explode('&', $rst_str);
+    $ary_user_data = array();
+    foreach ($ary_rst_str as $keyval) {
+        $ary_keyval = explode('=', $keyval);
+        $key = $ary_keyval[0];
+        $val = $ary_keyval[1];
+        $ary_user_data[$key] = $val;
+    }
+    return $ary_user_data;
+
+}
+
 function ECPayGenCheckMac(array $params, $hashKey, $hashIV, $encType = 1)
 {
     // 0) 如果資料中有 null，必需轉成空字串
